d467ffec8333b4e523d102fb43fbff7843472c20
Ricardo <linuxmei@foxmail.com>
2017-01-04 13:56:39 +0800

    a

--- src/js/papa.js
+++ src/js/papa.js
@@ -3,7 +3,9 @@ $(function(argument) {
     var url = "/src/js/flying.csv";
     Papa.parse(url, {
         download: true,
-        complete:function(){
+        complete:function(data){
+            var data = data[0].data;
+            store("data",data);
             console.log("isOk");
         }
         // rest of config ...
